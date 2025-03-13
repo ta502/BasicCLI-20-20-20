174f4494cf4225e21902681fcac3c4237bf57b36
ta502 <Tlal22336677@outlook.com>
2025-03-14 01:39:51 +0300

    fixing spelling of a word, and solving bug which made it count seconds not minutes.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -15,17 +15,17 @@ public class Main {
             int userInput = scan.nextInt();
             // Letting the user know what their interval looks, and notifying them that the program started
             System.out.println("Info: your interval is " + userInput + "-" + userInput + "-" + userInput);
-            System.out.println("Info: starting, ge1t ready.");
+            System.out.println("Info: starting, get ready.");
             TimeUnit.SECONDS.sleep(2);
             System.out.println("Info: Started.");
-            // start the loop
+            // Start the loop
             while (true) {
                 SystemTray tray = SystemTray.getSystemTray();
                 Image image = Toolkit.getDefaultToolkit().createImage("icon.png");
                 TrayIcon trayIcon = new TrayIcon(image, "BasicCLI-20-20-20");
                 trayIcon.setImageAutoSize(true);
                 tray.add(trayIcon);
-                TimeUnit.SECONDS.sleep(userInput);
+                TimeUnit.MINUTES.sleep(userInput);
                 System.out.println("Info: " + userInput + " minutes out, now look at something " + userInput + " feet " + "(approx " + userInput * 0.3048 + " meters)" + " away for " + userInput + " seconds!");
                 trayIcon.displayMessage("BasicCLI-20-20-20", userInput + " minutes out, now look at something " + userInput + " feet " + "(approx " + userInput * 0.3048 + " meters)" + " away for " + userInput + " seconds!", TrayIcon.MessageType.INFO);
                 TimeUnit.SECONDS.sleep(userInput);
